16a3468c355e5f8d31fa25bacba00afbbee35fba
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2024-10-17 19:37:41 +0300

    added garble -tiny -literals

--- Poshito/build.py
+++ Poshito/build.py
@@ -32,9 +32,10 @@ build_tags_list = [
     "asm"
 ]
 build_tags_cmd = f'-tags "{" ".join(build_tags_list)}"'
-pre_compile = "cd Agent && GOOS=windows GOARCH=amd64"
-compile_exe = f"{pre_compile} garble build -o ../{output_exe} {build_tags_cmd} ."
-compile_dll = f"{pre_compile} CGO_ENABLED=1 garble build -buildmode=c-shared -o ../{output_dll} {build_tags_cmd} . && rm ../*.h"
+pre_compile = "cd Agent && GOOS=windows GOARCH=amd64 CGO_ENABLED=1"
+garble_compile = "garble -tiny -literals build"
+compile_exe = f"{pre_compile} {garble_compile} -o ../{output_exe} {build_tags_cmd} ."
+compile_dll = f"{pre_compile} {garble_compile} -buildmode=c-shared -o ../{output_dll} {build_tags_cmd} . && rm ../*.h"
 upx_cmd = "upx -9 {}"
 dll_go_file = """
 package main
@@ -110,7 +111,7 @@ def main():
         with open("Agent/Dll.go", "wt") as f:
             f.write(dll_go_file.format(args.export_name))
     if args.no_garble:
-        compile_cmd = compile_cmd.replace("garble", "go")
+        compile_cmd = compile_cmd.replace(garble_compile, "go build")
 
     # Disable features if needed
     if args.disable_drm:
